92cc2bfc8b5c91a658a4336d908c2f67d8f428e8
AngelinaShoiliRozario <shoili.rozario@gmail.com>
2024-02-10 17:15:29 +0600

    ksdhf

--- app.py
+++ app.py
@@ -260,7 +260,7 @@ def index():
 
 @app.route('/video')
 def video():
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     cap.set(3, 640)
     cap.set(4, 480)
     return Response(detect_spoof(cap), mimetype='multipart/x-mixed-replace; boundary=frame')
